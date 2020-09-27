56005bf170c369f412ba86aed77f90f31d5e5a77
Malay Srivastava <57169971+malaysrivastava@users.noreply.github.com>
2020-09-27 15:16:10 +0530

    Express-session-left

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -13,4 +13,12 @@ router.get('/google/callback',passport.authenticate('google',{failureRedirect:
 '/'}),(req,res)=>{
     res.redirect('/dashboard')
 });
+
+//logout user
+
+router.get('/logout', (req,res)=>{
+    req.logout()
+    res.redirect('/')
+})
+
 module.exports = router;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 //login/landing page
 
-router.get('/',(req,res)=>{
+router.get('/',ensureGuest,(req,res)=>{
   res.render('login.hbs',{
     layout: 'login',
   })
@@ -11,7 +12,8 @@ router.get('/',(req,res)=>{
 
 //dashboardpage
 
-router.get('/dashboard',(req,res)=>{
+router.get('/dashboard',ensureAuth,(req,res)=>{
+  
   res.render('dashboard.hbs')
 });
 module.exports = router;
